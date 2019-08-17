25d153c466585d2cdb7b3c46d18adedfacf98d7d
AnishAggarwal71 <54200303+AnishAggarwal71@users.noreply.github.com>
2019-08-17 12:23:15 +0530

    Added codes

---
+++ Name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char x[50];int n;
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++)
+        scanf("%c",x[i]);
+    for(int J=0;J<=n;J++)
+        printf("Hi! %c",x[]);
+    return 0;
+}
