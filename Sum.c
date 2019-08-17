81c7d692c6e29e6bfe85a11406944ac691f3ca5e
AnishAggarwal71 <54200303+AnishAggarwal71@users.noreply.github.com>
2019-08-17 12:19:35 +0530

    Added Code

---
+++ Sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{  int a;
+   float b,c;
+   printf("Enter the first number");
+   scanf("%d",&a);
+   printf("Enter the second number");
+   scanf("%f",&b)
+   c = a+b;
+   printf("The sum is:",c);
+   return 0;
+}
