1073830aa50de39034c5c9bb27205294254ba2cd
AnishAggarwal71 <54200303+AnishAggarwal71@users.noreply.github.com>
2019-09-04 12:43:14 +0530

    Added Codes

---
+++ Sum_till_10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+			{
+			    int n=1,sum=0;
+			     printf("0");
+			    while (n<=10)
+                {
+
+               printf("+%d",n);
+               sum+=n;
+               n++;
+
+			  }
+			  printf("=%d",sum);
+            }
